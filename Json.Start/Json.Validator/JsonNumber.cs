89e9354388bf31c7664163fb7b47357c9902aacb
andreimusan <andreimusan@users.noreply.github.com>
2020-10-01 09:25:50 +0300

    restructure code

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -6,11 +6,6 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            if (HasContent(input) && StartsWithMinus(input))
-            {
-                input = input.Remove(0, 1);
-            }
-
             return HasContent(input)
                 && IsRealNumber(input)
                 && !StartsWithZero(input);
@@ -30,13 +25,14 @@ namespace Json
 
             for (int i = 0; i < input.Length; i++)
             {
-                if (!char.IsDigit(input[i]) && !CheckExponent(input, i) && input[i] != '.' || !CountElement(input))
+                if (!char.IsDigit(input[i]) && input[i] != '.' && !CheckExponent(input, i) && !HasPositiveAndNegative(input, i))
                 {
                     return false;
                 }
             }
 
-            return CheckExponentIndex(input);
+            return CountElement(input)
+                && CheckExponentIndex(input);
         }
 
         static bool StartsWithZero(string input)
@@ -44,15 +40,11 @@ namespace Json
             return input.Length > 1 && input[0] == '0' && input[1] != '.';
         }
 
-        static bool StartsWithMinus(string input)
-        {
-            return input.Length > 1 && input[0] == '-';
-        }
-
         static bool CountElement(string input)
         {
             int countDots = 0;
             int countExponent = 0;
+            int countOperator = 0;
 
             foreach (char c in input)
             {
@@ -65,19 +57,26 @@ namespace Json
                 {
                     countExponent++;
                 }
+
+                if (c == '-' || c == '+')
+                {
+                    countOperator++;
+                }
             }
 
-            return countDots <= 1 && countExponent <= 1;
+            return countDots <= 1 && countExponent <= 1 && countOperator <= 1 + 1;
         }
 
         static bool CheckExponent(string input, int index)
         {
-            if (index == input.Length - 1)
+            if (index == 0 || index == input.Length - 1)
             {
                 return false;
             }
 
-            return input[index] == 'e' || input[index] == 'E' || input[index] == '+' || input[index] == '-';
+            input = input.ToLower();
+
+            return input[index] == 'e';
         }
 
         static bool CheckExponentIndex(string input)
@@ -85,6 +84,22 @@ namespace Json
             input = input.ToLower();
 
             return input.IndexOf('.') < 0 || input.IndexOf('e') < 0 || input.IndexOf('.') <= input.IndexOf('e');
+        }
+
+        static bool HasPositiveAndNegative(string input, int index)
+        {
+            if (index == input.Length - 1)
+            {
+                return false;
+            }
+
+            input = input.ToLower();
+
+            var firstPosition = index == 0 && (input[index] == '-' || input[index] == '+');
+            var afterExponent = index > 1 && input[index - 1] == 'e' && (input[index] == '-' || input[index] == '+');
+
+            return firstPosition
+                || afterExponent;
         }
     }
 }
