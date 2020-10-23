f5a2c34b56eea991e419d36c75c46d554e01ebf7
andreimusan <andreimusan@users.noreply.github.com>
2020-10-23 15:21:41 +0300

    added class Range

---
+++ Range/RangeProblem/RangeProblem.Facts/RangeProblemFacts.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+
+namespace RangeProblem.Facts
+{
+    public class RangeProblemFacts
+    {
+        [Fact]
+        public void IsNotNull()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.False(digit.Match(null));
+        }
+
+        [Fact]
+        public void IsNotAnEmptyString()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.False(digit.Match(string.Empty));
+        }
+
+        [Fact]
+        public void IsAValidString()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.True(digit.Match("fab"));
+        }
+
+        [Fact]
+        public void IsNotAValidString()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.False(digit.Match("1ab"));
+        }
+    }
+}
---
+++ Range/RangeProblem/RangeProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RangeProblem
+{
+    public class Range
+    {
+        private readonly char start;
+        private readonly char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] < start || text[i] > end)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
