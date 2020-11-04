4ed40bde3aec5b7c296561e051f75aa9712e6280
andreimusan <andreimusan@users.noreply.github.com>
2020-11-04 17:13:29 +0200

    add class sequence

---
+++ Range/RangeProblem/RangeProblem.Facts/SequenceFacts.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeProblem.Facts
+{
+    public class SequenceFacts
+    {
+        [Fact]
+        public void IsNotNull()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var actual = ab.Match(null);
+            Assert.False(actual.Success());
+            Assert.Null(actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAnEmptyString()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var actual = ab.Match(String.Empty);
+            Assert.False(actual.Success());
+            Assert.Equal(String.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidStringSequence1()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var actual = ab.Match("abcd");
+            Assert.True(actual.Success());
+            Assert.Equal("cd", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidStringSequence1()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var actual = ab.Match("ax");
+            Assert.False(actual.Success());
+            Assert.Equal("ax", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidStringSequence2()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var actual = ab.Match("def");
+            Assert.False(actual.Success());
+            Assert.Equal("def", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidStringSequence3()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var actual = ab.Match("");
+            Assert.False(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidStringSequence2()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var abc = new Sequence(
+                ab,
+                new Character('c')
+            );
+
+            var actual = abc.Match("abcd");
+            Assert.True(actual.Success());
+            Assert.Equal("d", actual.RemainingText());
+        }
+    }
+}
---
+++ Range/RangeProblem/RangeProblem/Sequence.cs
@@ -0,0 +1,29 @@
+﻿namespace RangeProblem
+{
+    public class Sequence : IPattern
+    {
+        private readonly IPattern[] patterns;
+
+        public Sequence(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            var remainingText = text;
+
+            foreach (var pattern in patterns)
+            {
+                if (remainingText == null || !pattern.Match(remainingText).Success())
+                {
+                    return new FailedMatch(text);
+                }
+
+                remainingText = remainingText.Substring(1);
+            }
+
+            return new SuccessMatch(remainingText);
+        }
+    }
+}
