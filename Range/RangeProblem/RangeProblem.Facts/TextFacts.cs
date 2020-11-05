72cb3c5b83fb0f189da496c7dfddd61eb2e321b3
andreimusan <andreimusan@users.noreply.github.com>
2020-11-05 18:33:55 +0200

    added class text

---
+++ Range/RangeProblem/RangeProblem.Facts/TextFacts.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeProblem.Facts
+{
+    public class TextClassFacts
+    {
+        [Fact]
+        public void StringIsNotNull()
+        {
+            var trueValue = new Text("true");
+
+            var actual = trueValue.Match(null);
+
+            Assert.False(actual.Success());
+            Assert.Null(actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidTrueString1()
+        {
+            var trueValue = new Text("true");
+
+            var actual = trueValue.Match("true");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidTrueString2()
+        {
+            var trueValue = new Text("true");
+
+            var actual = trueValue.Match("trueX");
+
+            Assert.True(actual.Success());
+            Assert.Equal("X", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidTrueString1()
+        {
+            var trueValue = new Text("true");
+
+            var actual = trueValue.Match("false");
+
+            Assert.False(actual.Success());
+            Assert.Equal("false", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidTrueString2()
+        {
+            var trueValue = new Text("true");
+
+            var actual = trueValue.Match(String.Empty);
+
+            Assert.False(actual.Success());
+            Assert.Equal(String.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidFalseString1()
+        {
+            var falseValue = new Text("false");
+
+            var actual = falseValue.Match("false");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidFalseString2()
+        {
+            var falseValue = new Text("false");
+
+            var actual = falseValue.Match("falseX");
+
+            Assert.True(actual.Success());
+            Assert.Equal("X", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidFalseString()
+        {
+            var trueValue = new Text("false");
+
+            var actual = trueValue.Match("true");
+
+            Assert.False(actual.Success());
+            Assert.Equal("true", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidEmptyString()
+        {
+            var empty = new Text("");
+
+            var actual = empty.Match("true");
+
+            Assert.True(actual.Success());
+            Assert.Equal("true", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidEmptyString()
+        {
+            var empty = new Text("");
+
+            var actual = empty.Match(null);
+
+            Assert.False(actual.Success());
+            Assert.Null(actual.RemainingText());
+        }
+    }
+}
---
+++ Range/RangeProblem/RangeProblem/Text.cs
@@ -0,0 +1,19 @@
+﻿namespace RangeProblem
+{
+    public class Text : IPattern
+    {
+        private readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            return text?.Contains(prefix) == true && text.IndexOf(prefix) == 0
+                ? new SuccessMatch(text.Substring(prefix.Length))
+                : (IMatch)new FailedMatch(text);
+        }
+    }
+}
