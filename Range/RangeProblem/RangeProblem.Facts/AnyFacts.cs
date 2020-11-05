636d8000f48621e53dd0be2472d0389386a4107b
andreimusan <andreimusan@users.noreply.github.com>
2020-11-05 16:48:47 +0200

    added class any

---
+++ Range/RangeProblem/RangeProblem.Facts/AnyFacts.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeProblem.Facts
+{
+    public class AnyFacts
+    {
+        [Fact]
+        public void IsNotNull()
+        {
+            var e = new Any("eE");
+
+            var actual = e.Match(null);
+
+            Assert.False(actual.Success());
+            Assert.Null(actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAnEmptyString()
+        {
+            var e = new Any("eE");
+
+            var actual = e.Match(String.Empty);
+
+            Assert.False(actual.Success());
+            Assert.Equal(String.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString1()
+        {
+            var e = new Any("eE");
+
+            var actual = e.Match("ea");
+
+            Assert.True(actual.Success());
+            Assert.Equal("a", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString2()
+        {
+            var e = new Any("eE");
+
+            var actual = e.Match("Ea");
+
+            Assert.True(actual.Success());
+            Assert.Equal("a", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidString()
+        {
+            var e = new Any("eE");
+
+            var actual = e.Match("a");
+
+            Assert.False(actual.Success());
+            Assert.Equal("a", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotNullSign()
+        {
+            var sign = new Any("-+");
+
+            var actual = sign.Match(null);
+
+            Assert.False(actual.Success());
+            Assert.Null(actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAnEmptyStringWithSign()
+        {
+            var sign = new Any("-+");
+
+            var actual = sign.Match(String.Empty);
+
+            Assert.False(actual.Success());
+            Assert.Equal(String.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidStringWithSign1()
+        {
+            var sign = new Any("-+");
+
+            var actual = sign.Match("+3");
+
+            Assert.True(actual.Success());
+            Assert.Equal("3", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidStringWithSign2()
+        {
+            var sign = new Any("-+");
+
+            var actual = sign.Match("-2");
+
+            Assert.True(actual.Success());
+            Assert.Equal("2", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidStringWithSign()
+        {
+            var sign = new Any("-+");
+
+            var actual = sign.Match("2");
+
+            Assert.False(actual.Success());
+            Assert.Equal("2", actual.RemainingText());
+        }
+    }
+}
---
+++ Range/RangeProblem/RangeProblem/Any.cs
@@ -0,0 +1,25 @@
+﻿namespace RangeProblem
+{
+    public class Any : IPattern
+    {
+        private readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            foreach (var character in accepted)
+            {
+                if (!string.IsNullOrEmpty(text) && character == text[0])
+                {
+                    return new SuccessMatch(text.Substring(1));
+                }
+            }
+
+            return new FailedMatch(text);
+        }
+    }
+}
