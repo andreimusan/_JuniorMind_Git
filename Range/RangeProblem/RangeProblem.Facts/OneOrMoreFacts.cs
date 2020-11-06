8e17ab8acdc79a4981f0f54e9522f829480a5d1d
andreimusan <andreimusan@users.noreply.github.com>
2020-11-06 13:56:38 +0200

    added class OneOrMore

---
+++ Range/RangeProblem/RangeProblem.Facts/OneOrMoreFacts.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeProblem.Facts
+{
+    public class OneOrMoreFacts
+    {
+        [Fact]
+        public void IsNotNull()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            var actual = a.Match(null);
+
+            Assert.False(actual.Success());
+            Assert.Null(actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAnEmptyString()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            var actual = a.Match(String.Empty);
+
+            Assert.False(actual.Success());
+            Assert.Equal(String.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString1()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            var actual = a.Match("123");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString2()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            var actual = a.Match("1a");
+
+            Assert.True(actual.Success());
+            Assert.Equal("a", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString3()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            var actual = a.Match("12a");
+
+            Assert.True(actual.Success());
+            Assert.Equal("a", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNotAValidString()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            var actual = a.Match("bc");
+
+            Assert.False(actual.Success());
+            Assert.Equal("bc", actual.RemainingText());
+        }
+    }
+}
---
+++ Range/RangeProblem/RangeProblem/OneOrMore.cs
@@ -0,0 +1,17 @@
+﻿namespace RangeProblem
+{
+    public class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Sequence(new Choice(pattern), new Many(pattern));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
