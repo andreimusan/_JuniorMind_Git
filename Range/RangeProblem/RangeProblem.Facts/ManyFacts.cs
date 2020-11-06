26b29ed5fc459bb7ddeb295906e47ac059b0d69d
andreimusan <andreimusan@users.noreply.github.com>
2020-11-06 12:08:23 +0200

    added class Many

---
+++ Range/RangeProblem/RangeProblem.Facts/ManyFacts.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeProblem.Facts
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void IsAValidNull()
+        {
+            var a = new Many(new Character('a'));
+
+            var actual = a.Match(null);
+
+            Assert.True(actual.Success());
+            Assert.Null(actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidEmptyString()
+        {
+            var a = new Many(new Character('a'));
+
+            var actual = a.Match(String.Empty);
+
+            Assert.True(actual.Success());
+            Assert.Equal(String.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString1()
+        {
+            var a = new Many(new Character('a'));
+
+            var actual = a.Match("abc");
+
+            Assert.True(actual.Success());
+            Assert.Equal("bc", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString2()
+        {
+            var a = new Many(new Character('a'));
+
+            var actual = a.Match("aaaabc");
+
+            Assert.True(actual.Success());
+            Assert.Equal("bc", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString3()
+        {
+            var a = new Many(new Character('a'));
+
+            var actual = a.Match("bc");
+
+            Assert.True(actual.Success());
+            Assert.Equal("bc", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidStringWithDigits1()
+        {
+            var digits = new Many(new Range('0', '9'));
+
+            var actual = digits.Match("12345ab123");
+
+            Assert.True(actual.Success());
+            Assert.Equal("ab123", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidStringWithDigits2()
+        {
+            var digits = new Many(new Range('0', '9'));
+
+            var actual = digits.Match("ab");
+
+            Assert.True(actual.Success());
+            Assert.Equal("ab", actual.RemainingText());
+        }
+    }
+}
---
+++ Range/RangeProblem/RangeProblem/Many.cs
@@ -0,0 +1,24 @@
+﻿namespace RangeProblem
+{
+    public class Many
+    {
+        private readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            IMatch match = new SuccessMatch(text);
+
+            while (match.Success())
+            {
+                match = pattern.Match(match.RemainingText());
+            }
+
+            return new SuccessMatch(match.RemainingText());
+        }
+    }
+}
