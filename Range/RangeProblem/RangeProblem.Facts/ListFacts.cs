7ea9d1d0b040c015a2243cc7a5502da79c0b7c64
andreimusan <andreimusan@users.noreply.github.com>
2020-11-06 17:20:06 +0200

    added class List

---
+++ Range/RangeProblem/RangeProblem.Facts/ListFacts.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeProblem.Facts
+{
+    public class ListFacts
+    {
+        [Fact]
+        public void IsAValidNull()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            var actual = a.Match(null);
+
+            Assert.True(actual.Success());
+            Assert.Null(actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidEmptyString()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            var actual = a.Match(String.Empty);
+
+            Assert.True(actual.Success());
+            Assert.Equal(String.Empty, actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString1()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            var actual = a.Match("1,2,3");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString2()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            var actual = a.Match("1,2,3,");
+
+            Assert.True(actual.Success());
+            Assert.Equal(",", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString3()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            var actual = a.Match("1a");
+
+            Assert.True(actual.Success());
+            Assert.Equal("a", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidString4()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            var actual = a.Match("abc");
+
+            Assert.True(actual.Success());
+            Assert.Equal("abc", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidList1()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            var actual = list.Match("1; 22  ;\n 333 \t; 22");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidList2()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            var actual = list.Match("1 \n;");
+
+            Assert.True(actual.Success());
+            Assert.Equal(" \n;", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsAValidList3()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            var actual = list.Match("abc");
+
+            Assert.True(actual.Success());
+            Assert.Equal("abc", actual.RemainingText());
+        }
+    }
+}
---
+++ Range/RangeProblem/RangeProblem/List.cs
@@ -0,0 +1,17 @@
+﻿namespace RangeProblem
+{
+    public class List : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Many(new Choice(new Sequence(new Choice(element, separator), element), element));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
