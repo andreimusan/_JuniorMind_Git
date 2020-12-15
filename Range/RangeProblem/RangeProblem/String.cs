382f7091e1ec37b7a7630f39e26f7c4cf2f8801d
andreimusan <andreimusan@users.noreply.github.com>
2020-12-15 13:35:31 +0200

    changed class string

--- Range/RangeProblem/RangeProblem/String.cs
+++ Range/RangeProblem/RangeProblem/String.cs
@@ -11,15 +11,29 @@ namespace RangeProblem
             // aici construiește patternul pentru
             // un JSON string
             var quote = new Character('"');
-            var character = new Range(Convert.ToChar(0x20), Convert.ToChar(0xFFFF), "\"\\");
-            var characters = new Optional(new OneOrMore(character));
 
-            var escapeSeq = new List(characters, new Sequence(new Character('\\'), new Any("\"\\/bfnrt")));
+            var escapeSeq = new Sequence(
+                                new Character('\\'),
+                                new Any("\"\\/bfnrt"));
 
-            var hex = new Choice(new Range('0', '9', ""), new Range('a', 'f', ""), new Range('A', 'F', ""));
-            var hexSeq = new List(characters, new Sequence(new Character('\\'), new Character('u'), new Sequence(hex, hex, hex, hex)));
+            var hex = new Choice(
+                        new Range('0', '9'),
+                        new Range('a', 'f'),
+                        new Range('A', 'F'));
 
-            pattern = new Sequence(quote, characters, escapeSeq, hexSeq, quote);
+            var hexSeq = new Sequence(
+                            new Character('\\'),
+                            new Character('u'),
+                            new Sequence(hex, hex, hex, hex));
+
+            var character = new Choice(
+                                new Range(Convert.ToChar(0x20), Convert.ToChar(0xFFFF), "\"\\"),
+                                escapeSeq,
+                                hexSeq);
+
+            var characters = new Many(character);
+
+            pattern = new Sequence(quote, characters, quote);
         }
 
         public IMatch Match(string text)
