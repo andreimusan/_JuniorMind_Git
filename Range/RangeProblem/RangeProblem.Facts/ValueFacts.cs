242f26ae4ec44bc64f5829200826817a0c760823
andreimusan <andreimusan@users.noreply.github.com>
2021-01-31 19:01:43 +0200

    changed class value

---
+++ Range/RangeProblem/RangeProblem.Facts/ValueFacts.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeProblem.Facts
+{
+    public class ValueFacts
+    {
+        [Fact]
+        public void IsNumberZero()
+        {
+            var value = new Value();
+
+            var actual = value.Match("0");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsNumber()
+        {
+            var value = new Value();
+
+            var actual = value.Match("-26");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsString()
+        {
+            var value = new Value();
+
+            var actual = value.Match("\"abc\"");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsTextTrue()
+        {
+            var value = new Value();
+
+            var actual = value.Match("true");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsTextFalse()
+        {
+            var value = new Value();
+
+            var actual = value.Match("false");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsTextNull()
+        {
+            var value = new Value();
+
+            var actual = value.Match("null");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsSimpleArray()
+        {
+            var value = new Value();
+
+            var actual = value.Match("[12]");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsComplexArray()
+        {
+            var value = new Value();
+
+            var actual = value.Match("[12, \"value\", 4, true]");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsEmptyArray()
+        {
+            var value = new Value();
+
+            var actual = value.Match("[ ]");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsSimpleObject()
+        {
+            var value = new Value();
+
+            var actual = value.Match("{\"value\": \"New\"}");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsComplexObject()
+        {
+            var value = new Value();
+
+            var actual = value.Match("{\"value\": \"Open\", \"onclick\": \"OpenDoc()\"}");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+
+        [Fact]
+        public void IsEmptyObject()
+        {
+            var value = new Value();
+
+            var actual = value.Match("{ \r }");
+
+            Assert.True(actual.Success());
+            Assert.Equal("", actual.RemainingText());
+        }
+    }
+}
---
+++ Range/RangeProblem/RangeProblem/Value.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RangeProblem
+{
+    public class Value : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+            // aici construiește patternul pentru
+            // un JSON value
+            // ai grijă să ți seama de `spații albe`
+            var stringValue = new String();
+            var number = new Number();
+
+            var value = new Choice(
+                            stringValue,
+                            number,
+                            new Text("true"),
+                            new Text("false"),
+                            new Text("null"));
+
+            var quote = new Character('"');
+            var openBracket = new Character('[');
+            var closeBracket = new Character(']');
+            var whitespace = new Choice(new Sequence(quote, quote), new Many(new Any(" \r\n\t")));
+            var separator = new Sequence(whitespace, new Character(','), whitespace);
+
+            var element = new Sequence(whitespace, value, whitespace);
+            var elements = new List(element, separator);
+
+            var array = new Choice(new Sequence(openBracket, whitespace, closeBracket), new Sequence(openBracket, elements, closeBracket));
+            value.Add(array);
+
+            var openCurlyBracket = new Character('{');
+            var closeCurlyBracket = new Character('}');
+
+            var member = new Sequence(whitespace, stringValue, whitespace, new Character(':'), element);
+            var members = new List(member, separator);
+
+            var objectValue = new Choice(new Sequence(openCurlyBracket, whitespace, closeCurlyBracket), new Sequence(openCurlyBracket, members, closeCurlyBracket));
+            value.Add(objectValue);
+
+            pattern = value;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
