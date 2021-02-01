13a7adb0054484c0c6e23a2edc505346b2d751bc
andreimusan <andreimusan@users.noreply.github.com>
2021-02-01 11:07:12 +0200

    added class Program, Main

---
+++ Range/RangeProblem/RangeProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RangeProblem
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\andre\OneDrive\Documente\DOCS\_JuniorMind\JSON_file.txt");
+            var value = new Value();
+            var actual = value.Match(text);
+
+            Console.WriteLine("The selected file is a valid JSON file? {0}", actual.Success());
+            Console.Read();
+        }
+    }
+}
