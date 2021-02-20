a1840dae5d3aa25815c4ca50a1dda12611f9a8dc
andreimusan <andreimusan@users.noreply.github.com>
2021-02-20 19:17:37 +0200

    added SortedList

---
+++ Colectii de date/Arrays/SortedList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public class SortedList<T> : List<T>
+        where T : IComparable<T>
+    {
+        public SortedList()
+            : base()
+        {
+        }
+
+        public override T this[int index]
+        {
+            get => base[index];
+            set
+            {
+                if (ElementAt(index - 1, value).CompareTo(value) <= 0 && value.CompareTo(ElementAt(index + 1, value)) <= 0)
+                {
+                    base[index] = value;
+                }
+            }
+        }
+
+        public override void Add(T element)
+        {
+            base.Add(element);
+            SortArray();
+        }
+
+        public override void Insert(int index, T element)
+        {
+            if (ElementAt(index - 1, element).CompareTo(element) <= 0 && element.CompareTo(ElementAt(index, element)) <= 0)
+            {
+                base.Insert(index, element);
+            }
+        }
+
+        private void SortArray()
+        {
+            for (int i = Count - 1; i > 0; i--)
+            {
+                if (base[i].CompareTo(base[i - 1]) < 0)
+                {
+                    Swap(i, i - 1);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            T temp = base[firstIndex];
+            base[firstIndex] = base[secondIndex];
+            base[secondIndex] = temp;
+        }
+
+        private T ElementAt(int index, T defaultValue)
+        {
+            return index < 0 || index >= Count ? defaultValue : base[index];
+        }
+
+        private int CompareTo(T operand1, T operand2)
+        {
+            if (operand1 == null || operand2 == null)
+            {
+                return 1;
+            }
+
+            return operand1.CompareTo(operand2);
+        }
+    }
+}
---
+++ Colectii de date/IntArray.Facts/SortedListFacts.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Xunit;
+
+namespace Arrays.Facts
+{
+    public class SortedListFacts
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            var list = new SortedList<int>() { 3, 20, 5, 2, 100 };
+
+            Assert.Equal(5, list.Count);
+            Assert.Equal(20, list[3]);
+            Assert.Equal(2, list[0]);
+        }
+
+        [Fact]
+        public void TestCount()
+        {
+            var list = new SortedList<int>() { 3, 5, 7 };
+
+            Assert.Equal(3, list.Count);
+        }
+
+        [Fact]
+        public void TestElement()
+        {
+            var list = new SortedList<int>() { 3, 20, 7, 5 };
+
+            Assert.Equal(5, list[1]);
+        }
+
+        [Fact]
+        public void TestSetElementTrue()
+        {
+            var list = new SortedList<int>() { 3, 7, 5 };
+            list[1] = 4;
+
+            Assert.Equal(4, list[1]);
+        }
+
+        [Fact]
+        public void TestSetElementTrue2()
+        {
+            var list = new SortedList<int>() { 3 };
+            list[1] = 2;
+
+            Assert.Equal(1, list.Count);
+        }
+
+        [Fact]
+        public void TestSetElementFalse()
+        {
+            var list = new SortedList<int>() { 3, 5, 7 };
+            list[1] = 20;
+
+            Assert.Equal(5, list[1]);
+        }
+
+        [Fact]
+        public void TestContainsTrue()
+        {
+            var list = new SortedList<int>() { 3, 5, 7, 20 };
+
+            Assert.True(list.Contains(20));
+        }
+
+        [Fact]
+        public void TestContainsFalse()
+        {
+            var list = new SortedList<int>() { 3, 5, 7 };
+            list[1] = 20;
+
+            Assert.False(list.Contains(20));
+        }
+
+        [Fact]
+        public void TestIndexOfExistingElement()
+        {
+            var list = new SortedList<int>() { 3, 5, 7, 20 };
+
+            Assert.Equal(3, list.IndexOf(20));
+        }
+
+        [Fact]
+        public void TestIndexOfNotExistingElement()
+        {
+            var list = new SortedList<int>() { 3, 5, 7 };
+            list[1] = 20;
+
+            Assert.Equal(-1, list.IndexOf(20));
+        }
+
+        [Fact]
+        public void TestInsertTrue()
+        {
+            var list = new SortedList<int>() { 3, 5, 7 };
+            list.Insert(1, 4);
+
+            Assert.Equal(4, list.Count);
+            Assert.True(list.Contains(4));
+            Assert.Equal(1, list.IndexOf(4));
+        }
+
+        [Fact]
+        public void TestInsertFalse()
+        {
+            var list = new SortedList<int>() { 3, 5, 7 };
+            list.Insert(0, 20);
+
+            Assert.Equal(3, list.Count);
+            Assert.False(list.Contains(20));
+        }
+
+        [Fact]
+        public void TestClear()
+        {
+            var list = new SortedList<int>() { 3, 5, 7 };
+            list.Clear();
+
+            Assert.Equal(0, list.Count);
+            Assert.False(list.Contains(5));
+        }
+
+        [Fact]
+        public void TestRemove()
+        {
+            var list = new SortedList<int>() { 7, 5, 3, 7, 30 };
+            list.Remove(7);
+            list.Add(20);
+
+            Assert.Equal(5, list.Count);
+            Assert.Equal(5, list[1]);
+            Assert.Equal(20, list[3]);
+        }
+
+        [Fact]
+        public void TestRemoveAt()
+        {
+            var list = new SortedList<int>() { 7, 5, 3, 7, 20, 30 };
+            list.RemoveAt(3);
+            list.Add(40);
+
+            Assert.Equal(6, list.Count);
+            Assert.Equal(3, list.IndexOf(20));
+            Assert.Equal(5, list.IndexOf(40));
+        }
+    }
+}
