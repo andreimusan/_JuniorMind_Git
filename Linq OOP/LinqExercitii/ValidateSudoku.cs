6c50270dc7753a66fad95fe974c4bb6a8b07cbc4
andreimusan <andreimusan_13@ymail.com>
2021-09-27 20:05:07 +0300

    added ValidateSudoku

---
+++ Linq OOP/LinqExercitii/ValidateSudoku.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqExercitii
+{
+    public class ValidateSudoku
+    {
+        public static bool IsValidSudokuBoard(byte[,] sudokuBoard)
+        {
+            var sudokuLineSize = 9;
+
+            if (sudokuBoard is null || sudokuBoard.GetLength(0) != sudokuLineSize || sudokuBoard.GetLength(1) != sudokuLineSize)
+            {
+                return false;
+            }
+
+            var sudokuLines = GenerateSudokuLines(sudokuBoard);
+            var sudokuColumns = GenerateSudokuColumns(sudokuBoard);
+            var sudokuBlocks = GenerateSudokuColumns(sudokuBoard);
+
+            var result = IsValidSudokuItem(sudokuLines) && IsValidSudokuItem(sudokuColumns) && IsValidSudokuItem(sudokuBlocks);
+
+            return result;
+        }
+
+        public static bool IsValidSudokuItem(IEnumerable<IEnumerable<byte>> itemType)
+        {
+            var result = itemType.Count(item => item.Where(value => value >= 1 && value <= 9).Distinct().Count() == 9) == itemType.Count();
+
+            return result;
+        }
+
+        private static IEnumerable<IEnumerable<byte>> GenerateSudokuLines(byte[,] sudokuBoard)
+        {
+            var result = Enumerable.Range(0, sudokuBoard.GetLength(0)).
+                Select(line => Enumerable.Range(0, sudokuBoard.GetLength(1)).
+                Select(column => sudokuBoard[line, column]));
+
+            return result;
+        }
+
+        private static IEnumerable<IEnumerable<byte>> GenerateSudokuColumns(byte[,] sudokuBoard)
+        {
+            var result = Enumerable.Range(0, sudokuBoard.GetLength(0)).
+                Select(line => Enumerable.Range(0, sudokuBoard.GetLength(1)).
+                Select(column => sudokuBoard[column, line]));
+
+            return result;
+        }
+
+        private static IEnumerable<IEnumerable<byte>> GenerateSudokuBlocks(byte[,] sudokuBoard)
+        {
+            var sudokuBlockSize = 3;
+            var result = Enumerable.Range(0, sudokuBoard.GetLength(0)).
+                Select(line => Enumerable.Range(0, sudokuBoard.GetLength(1)).
+                Select(column => sudokuBoard[line / sudokuBlockSize * sudokuBlockSize + column / sudokuBlockSize, line % sudokuBlockSize * sudokuBlockSize + column % sudokuBlockSize]));
+
+            return result;
+        }
+    }
+}
---
+++ Linq OOP/LinqExeritii.Facts/ValidateSudokuFacts.cs
@@ -0,0 +1,88 @@
+﻿using LinqExercitii;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace LinqExeritii.Facts
+{
+    public class ValidateSudokuFacts
+    {
+        [Fact]
+        public void Test_ValidateSudokuValidBoard()
+        {
+            byte[,] sudokuBoard = new byte[,]
+            {
+                { 6, 3, 9, 5, 7, 4, 1, 8, 2 },
+                { 5, 4, 1, 8, 2, 9, 3, 7, 6 },
+                { 7, 8, 2, 6, 1, 3, 9, 5, 4 },
+                { 1, 9, 8, 4, 6, 7, 5, 2, 3 },
+                { 3, 6, 5, 9, 8, 2, 4, 1, 7 },
+                { 4, 2, 7, 1, 3, 5, 8, 6, 9 },
+                { 9, 5, 6, 7, 4, 8, 2, 3, 1 },
+                { 8, 1, 3, 2, 9, 6, 7, 4, 5 },
+                { 2, 7, 4, 3, 5, 1, 6, 9, 8 }
+            };
+
+            Assert.True(ValidateSudoku.IsValidSudokuBoard(sudokuBoard));
+        }
+
+        [Fact]
+        public void Test_ValidateSudokuMoreLines()
+        {
+            byte[,] sudokuBoard = new byte[,]
+            {
+                { 6, 3, 9, 5, 7, 4, 1, 8, 2 },
+                { 5, 4, 1, 8, 2, 9, 3, 7, 6 },
+                { 7, 8, 2, 6, 1, 3, 9, 5, 4 },
+                { 1, 9, 8, 4, 6, 7, 5, 2, 3 },
+                { 3, 6, 5, 9, 8, 2, 4, 1, 7 },
+                { 4, 2, 7, 1, 3, 5, 8, 6, 9 },
+                { 9, 5, 6, 7, 4, 8, 2, 3, 1 },
+                { 8, 1, 3, 2, 9, 6, 7, 4, 5 },
+                { 2, 7, 4, 3, 5, 1, 6, 9, 8 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+            };
+
+            Assert.False(ValidateSudoku.IsValidSudokuBoard(sudokuBoard));
+        }
+
+        [Fact]
+        public void Test_ValidateSudokuMoreColumns()
+        {
+            byte[,] sudokuBoard = new byte[,]
+            {
+                { 6, 3, 9, 5, 7, 4, 1, 8, 2, 1 },
+                { 5, 4, 1, 8, 2, 9, 3, 7, 6, 2 },
+                { 7, 8, 2, 6, 1, 3, 9, 5, 4, 3 },
+                { 1, 9, 8, 4, 6, 7, 5, 2, 3, 4 },
+                { 3, 6, 5, 9, 8, 2, 4, 1, 7, 5 },
+                { 4, 2, 7, 1, 3, 5, 8, 6, 9, 6 },
+                { 9, 5, 6, 7, 4, 8, 2, 3, 1, 7 },
+                { 8, 1, 3, 2, 9, 6, 7, 4, 5, 8 },
+                { 2, 7, 4, 3, 5, 1, 6, 9, 8, 9 }
+            };
+
+            Assert.False(ValidateSudoku.IsValidSudokuBoard(sudokuBoard));
+        }
+
+        [Fact]
+        public void Test_ValidateSudokuDuplicateValues()
+        {
+            byte[,] sudokuBoard = new byte[,]
+            {
+                { 6, 3, 9, 5, 7, 4, 1, 8, 2, 1 },
+                { 5, 4, 1, 8, 2, 9, 3, 7, 6, 2 },
+                { 7, 8, 2, 6, 1, 3, 9, 5, 4, 3 },
+                { 1, 9, 8, 4, 6, 7, 5, 2, 3, 4 },
+                { 3, 6, 5, 9, 8, 2, 4, 1, 7, 7 },
+                { 4, 2, 7, 1, 3, 5, 8, 6, 9, 6 },
+                { 9, 5, 6, 7, 4, 8, 2, 3, 1, 7 },
+                { 8, 1, 3, 2, 9, 6, 7, 4, 5, 8 }
+            };
+
+            Assert.False(ValidateSudoku.IsValidSudokuBoard(sudokuBoard));
+        }
+    }
+}
