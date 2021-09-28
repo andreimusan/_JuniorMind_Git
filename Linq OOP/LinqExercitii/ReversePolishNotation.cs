7575f54e243440ef26ac71492bcb98d53033bb5d
andreimusan <andreimusan_13@ymail.com>
2021-09-28 18:26:33 +0300

    added reverse polish notation

---
+++ Linq OOP/LinqExercitii/ReversePolishNotation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqExercitii
+{
+    public class ReversePolishNotation
+    {
+        public static double PolishNotation(string operation)
+        {
+            if (operation == null)
+            {
+                return 0;
+            }
+
+            var operationArray = operation.Split(' ');
+
+            var result = operationArray.Aggregate(
+                new List<double>().AsEnumerable(), (operand, current) => double.TryParse(current, out var value) ?
+                    operand.Append(value) :
+                    operand.SkipLast(2).Append(OperationResult(operand.TakeLast(2), current))).First();
+
+            return result;
+        }
+
+        private static double OperationResult(IEnumerable<double> numbers, string value)
+        {
+            double result = 0;
+
+            var firstNumber = numbers.ElementAt(0);
+            var secondNumber = numbers.ElementAt(1);
+
+            result = value switch
+            {
+                "+" => firstNumber + secondNumber,
+                "-" => firstNumber - secondNumber,
+                "*" => firstNumber * secondNumber,
+                "/" => firstNumber / secondNumber,
+                "%" => firstNumber % secondNumber,
+                _ => result
+            };
+
+            return result;
+        }
+    }
+}
---
+++ Linq OOP/LinqExeritii.Facts/ReversePolishNotationFacts.cs
@@ -0,0 +1,35 @@
+﻿using LinqExercitii;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LinqExeritii.Facts
+{
+    public class ReversePolishNotationFacts
+    {
+        [Fact]
+        public void Test_ReversePolishNotationSimple()
+        {
+            var expression = "3 4 + 5 2 - *";
+
+            Assert.Equal(21, ReversePolishNotation.PolishNotation(expression));
+        }
+
+        [Fact]
+        public void Test_ReversePolishNotationMedium()
+        {
+            var expression = "5 1 2 + 4 * + 3 -";
+
+            Assert.Equal(14, ReversePolishNotation.PolishNotation(expression));
+        }
+
+        [Fact]
+        public void Test_ReversePolishNotationComplex()
+        {
+            var expression = "15 7 1 1 + - / 3 * 2 1 1 + + -";
+
+            Assert.Equal(5, ReversePolishNotation.PolishNotation(expression));
+        }
+    }
+}
