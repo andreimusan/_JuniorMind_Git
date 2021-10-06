bc4a7a98ef5ad1a008e8c5d44dafbbdc88ee608e
andreimusan <andreimusan_13@ymail.com>
2021-10-06 23:03:03 +0300

    added input and output folder, changed to read text from file

--- MermaidFlowChart/Program.cs
+++ MermaidFlowChart/Program.cs
@@ -5,17 +5,22 @@ using System.Linq;
 
 namespace MermaidFlowChart
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            var rectangles = new List<IFlowChartShape>
+            if (args == null || args.Length == 0)
             {
-                new RectangleNode("Tastez"),
-                new RectangleNode("Scriu"),
-                new RectangleNode("Multe cuvinte lungi"),
-                new RectangleNode("Eu"),
-            };
+                Console.WriteLine("Error: please specify the file to read!");
+                Console.ReadKey();
+                return;
+            }
+
+            string text = File.ReadAllText(args[0]);
+            var lines = text.Split("\n\r".ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var rectangles = new List<IFlowChartShape>();
+            Array.ForEach(lines, x => rectangles.Add(new RectangleNode(x)));
 
             rectangles.ForEach(x => x.UpdateWidth());
             int maxLenghth = rectangles.OrderBy(x => x.GetDimensions().width).Last().GetDimensions().width;
@@ -31,10 +36,8 @@ namespace MermaidFlowChart
                 prevHeight += currentHeight;
             }
 
-            string file = "<svg width = \"1000\" height = \"1000\" xmlns = \"http://www.w3.org/2000/svg\" xmlns:svg = \"http://www.w3.org/2000/svg\">" +
-                          svg +
-                          "</svg>";
-            StreamWriter write = File.CreateText(@"drawing.svg");
+            string file = "<svg width = \"1000\" height = \"1000\" xmlns = \"http://www.w3.org/2000/svg\" xmlns:svg = \"http://www.w3.org/2000/svg\">" + svg + "</svg>";
+            StreamWriter write = File.CreateText(@"test_diagrams\drawing.svg");
             write.Write(file);
             write.Close();
         }
